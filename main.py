cb60b832d9f0d5361e0aa63487369c62d5d8156a
jbbbr <jean-bastien.ricot@ensae.fr>
2025-11-04 10:47:17 +0000

    del

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
 print("hello world")
-afjpiaef
-
-,;ntkz
-test
-bonjour Jean-Baptiste, as-tu des idées pour une variable qui puisse être corrêlée avec l'offre de loisir en France ? 
-
-Mille pardons Jean ! 
